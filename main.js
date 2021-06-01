d7925b2a4c2aad62c5dc1dbaa3cfbfb8e7119c3e
manasvijain20 <74360258+manasvijain20@users.noreply.github.com>
2021-06-01 19:03:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+
+nose_x = 0;
+nose_y = 0;
+function preload(){
+    clown_nose = loadImage("https://i.postimg.cc/PxFvYgkv/l1.png");
+}
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelloaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelloaded(){
+    console.log('poseNet is initialized');
+}
+function draw(){
+    image(video, 0,0,300,300);
+    image(clown_nose, nose_x, nose_y, 30, 30);
+}
+function take_snapshot(){
+    save('image.png');
+}
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        nose_x = results[0].pose.nose.x-15;
+        nose_y = results[0].pose.nose.y+13;
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+    }
+}
+
